be47b1ee37e7789f15fcca10142b317944973dbc
Nader Abu Saad <nabusaad@yahoo.com>
2020-02-16 21:53:52 +0200

    Added new Activity

--- app/src/main/java/com/example/buttonclickevents/MainActivity.java
+++ app/src/main/java/com/example/buttonclickevents/MainActivity.java
@@ -6,12 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 implements View.OnClickListener {
 
     TextView txtView;
-    Button button1, button2;
+    Button button1, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +25,19 @@ implements View.OnClickListener {
 
         button2 = findViewById(R.id.button2);
         button2.setOnClickListener(this);
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast toast = Toast. makeText(getApplicationContext(),
+                        "Welcome to Android",Toast.LENGTH_LONG);
+                toast.setMargin(50,450);
+                toast.show();
+            }
+        });
     }
 
     @Override
